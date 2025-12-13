07ee643c33d05592f9a8e5b6062ce2e4335db758
Anjana P R <pranjana333@gmail.com>
2025-12-13 12:44:17 +0530

    Swapping number problem

---
+++ Programiz_Practice.js
@@ -0,0 +1,8 @@
+// Qn-1 JavaScript Program To Print Hello World
+// console.log("Hello World!")
+
+// Qn-2 Add two numbers
+let a = parseInt(prompt("Enter the first number: ", 10);
+let b = parseInt(prompt("Enter the second number: "), 10);
+let sum = a + b;
+console.log("The sum of numbers is" + sum);
