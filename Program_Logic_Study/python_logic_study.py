3441d223f4a9f41259de4515f024c0dcba69cd77
Anjana P R <pranjana333@gmail.com>
2025-12-13 16:05:33 +0530

    Add JavaScript programs for finding the largest of three numbers and checking prime numbers; create new Python and HTML files for logic study

---
+++ Program_Logic_Study/python_logic_study.py
@@ -0,0 +1,8 @@
+# 1. Check eligible to vote (age ≥ 18)
+# age = int(input("Enter your age:"))
+# if age >= 18:
+#     print("Eligible for voting")
+# else:
+#     print("Not eligible for voting")
+
+# 2. Find largest of three numbers
--- Programiz_Practice.js
+++ Programiz_Practice.js
@@ -42,3 +42,19 @@
         // else{
         //     console.log("The number is odd")
         // }
+
+// Qn-12 JavaScript Program to Find the Largest Among Three Numbers
+        // let a = 10;
+        // let b = 20;
+        // let c = 30;
+        // if ((a>b) && (a>c)){
+        //     console.log(a + " is the largest number");
+        // }
+        // else if ((b>a) && (b>c)){
+        //     console.log(b + " is the largest number");
+        // }
+        // else{
+        //     console.log(c + " is the largest number");
+        // }
+
+// Qn-13 JavaScript Program to Check Prime Number
