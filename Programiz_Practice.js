11189f730a47113cd9a2c4d9a2887035e62623d8
Anjana P R <pranjana333@gmail.com>
2025-12-13 13:33:03 +0530

    Refactor JavaScript programs to use prompts for user input and comment out example code

--- Programiz_Practice.js
+++ Programiz_Practice.js
@@ -2,7 +2,43 @@
 // console.log("Hello World!")
 
 // Qn-2 Add two numbers
-let a = parseInt(prompt("Enter the first number: ", 10);
-let b = parseInt(prompt("Enter the second number: "), 10);
-let sum = a + b;
-console.log("The sum of numbers is" + sum);
+// let a = 1;
+// let b = 2;
+// let sum = a + b;
+// console.log("The sum of numbers is" + sum);
+
+// Qn-5 JavaScript Program to Swap Two Variables
+// let a = 1;
+// let b = 2;
+// let temp;
+// console.log("The numbers before swapping is " +a+ " and " + b );
+// // temp = a;
+// // a = b;
+// // b = temp;
+  
+// a = a + b;
+// b = a - b;
+// a = a - b;
+// console.log("The numbers after swapping is " +a+ " and " + b );
+
+// Qn-10 Javascript Program to Check if a number is Positive, Negative, or Zero
+// let num = 100
+//         if (num > 0){
+//             console.log("The number is positive")
+//         }
+//         else if (num < 0){
+//             console.log("The number is negative")
+//         }
+//         else{
+//             console.log("The number is zero")
+//         }
+
+
+// Qn-11 Javascript Program to Check if a Number is Odd or Even
+        // num = 50
+        // if((num % 2) == 0){
+        //     console.log("The number is even")
+        // }
+        // else{
+        //     console.log("The number is odd")
+        // }
