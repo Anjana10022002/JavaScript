b1ec87baa9230ea6a406e74214e880d4be048e53
Anjana P R <pranjana333@gmail.com>
2025-12-15 00:59:59 +0530

    Fix sum output formatting in Program Logic Study

--- Programiz_Practice.js
+++ Programiz_Practice.js
@@ -58,3 +58,86 @@
         // }
 
 // Qn-13 JavaScript Program to Check Prime Number
+// const number = parseInt(prompt("Enter a positive number: "));
+// let isPrime = true;
+// if (number === 1) {
+//     console.log("1 is neither prime nor composite number.");
+// }
+// else if (number > 1) {
+//     for (let i = 2; i <= number/2; i++) {
+//         if (number % i == 0) {
+//             isPrime = false;
+//             break;
+//         }
+//     }
+//     if (isPrime) {
+//         console.log(`${number} is a prime number`);
+//     } else {
+//         console.log(`${number} is a not prime number`);
+//     }
+// }
+// else {
+//     console.log("The number is not a prime number.");
+// }
+
+// Qn-14 JavaScript Program to Print All Prime Numbers in an Interval
+// const lowerNumber = parseInt(prompt("Enter the lower number: "));
+// const higherNumber = parseInt(prompt("Enter the higher number: "));     
+// console.log(`Prime numbers between ${lowerNumber} and ${higherNumber} are:`);
+
+// for (let i = lowerNumber; i <= higherNumber; i++) {
+//         let isPrime = true;
+//         if (i === 1) {
+//                 isPrime = false;
+//         }
+//         else {
+//             for (let j = 2; j <= i / 2; j++) {
+//                 if (i % j == 0) {
+//                     isPrime = false;
+//                     break;
+//                 }
+//             }
+//         }
+//         if (isPrime) {
+//                 console.log(i);
+//         }
+// }
+
+// Qn-15 JavaScript Program to Find the Factorial of a Number
+// const number = parseInt(prompt("Enter a positive integer: "));
+// let factorial = 1;   
+// if (number < 0) {
+//     console.log("Error! Factorial for negative number does not exist.");
+// }
+// else if (number === 0) {
+//     console.log(`The factorial of 0 is 1.`);
+// }
+// else {
+//     for (let i = 1; i <= number; i++) {
+//         factorial *= i;
+//     }
+//     console.log(`The factorial of ${number} is ${factorial}.`);
+// }
+
+
+// Qn-16 JavaScript Program to Display the Multiplication Table
+// const number = parseInt(prompt("Enter a number to display its multiplication table: "));
+// console.log(`Multiplication table of ${number}:`);
+// for (let i = 1; i <= 10; i++) {
+//     const result = number * i;
+//     console.log(`${number} x ${i} = ${result}`);
+// }
+
+
+// Qn-17 JavaScript Program to Generate Fibonacci Series
+// const nterms = parseInt(prompt("Enter the number of terms: "));
+// let n1 = 0, n2 = 1, nextTerm;
+// console.log("Fibonacci Series:");
+// for (let i = 1; i <= nterms; i++) {
+//     console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
+
+
