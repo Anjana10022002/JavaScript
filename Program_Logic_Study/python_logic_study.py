f03922131b22df3b2b2b8b7b7758075b24caad39
Anjana P R <pranjana333@gmail.com>
2025-12-14 18:24:32 +0530

    Sum of n  numbers

--- Program_Logic_Study/python_logic_study.py
+++ Program_Logic_Study/python_logic_study.py
@@ -23,3 +23,10 @@
 #     print(a, "is a leap year")
 # else:
 #     print(a, "is not a leap year")
+
+# 4. Sum of First N Numbers
+n = int(input("Enter n:"))
+sum = 0
+for i in range(1, n + 1):
+    sum = sum + i
+print("Sum is: ",sum)
