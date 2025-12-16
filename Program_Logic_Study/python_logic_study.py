4d086f58b064b48eaaccc241afad6fe1815a341a
Anjana P R <pranjana333@gmail.com>
2025-12-16 23:23:05 +0530

    Reversing a number

--- Program_Logic_Study/python_logic_study.py
+++ Program_Logic_Study/python_logic_study.py
@@ -37,3 +37,10 @@
 # for i in range (1, n+1):
 #     fact = fact * i
 # print("The factorial is: ", fact)
+
+# 6. Reverse a string 
+n = int(input("Enter the number: "))
+rev = 0
+while n > 0:
+    digit = n % 10
+    rev = rev * 10 + digit
