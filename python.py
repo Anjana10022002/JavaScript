fb4b569b5a18f9253a27e959da0ae4e52727a370
Anjana P R <pranjana333@gmail.com>
2025-12-30 23:25:08 +0530

    Checking  a number is palindrome or not

--- python.py
+++ python.py
@@ -1,10 +1,20 @@
-num = int(input("Enter a number: "))
-# 123
-rev = 0
-while num > 0:
-    digit = num % 10
-    rev = rev * 10 + digit
-    num //= 10
-print("Reverse:", rev)
+# num = int(input("Enter a number: "))
+# rev = 0
+# while num > 0:
+#     digit = num % 10
+#     rev = rev * 10 + digit
+#     num //= 10
+# print("Reverse:", rev)
 
     
+num = int(input("Enter the number: "))
+temp = num
+num = 0
+while num > 0:
+    digit = num % 10
+    rev = digit * 10 + rev 
+    num //= 10
+if temp == rev:
+    print("The number is palindrome ")
+else:
+    print("The number is not palindrome.")
