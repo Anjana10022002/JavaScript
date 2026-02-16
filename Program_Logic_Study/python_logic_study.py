5f4f0172336259fa36afaa4f4c8b5109e79eae22
Anjana P R <pranjana333@gmail.com>
2026-02-16 23:59:41 +0530

    second largest number in an array

--- Program_Logic_Study/python_logic_study.py
+++ Program_Logic_Study/python_logic_study.py
@@ -443,20 +443,20 @@
 #     print("Not Anagram")
 
 # prime number check
-n = 5
-if n <= 1:
-    print("not prime")
-else:
-    is_prime = True
-    for i in range (2, n):
-        if n % i == 0:
-            is_prime = False
-            break
+# n = 5
+# if n <= 1:
+#     print("not prime")
+# else:
+#     is_prime = True
+#     for i in range (2, n):
+#         if n % i == 0:
+#             is_prime = False
+#             break
 
-    if is_prime:
-        print("Prime")
-    else:
-        print("Not Prime")
+#     if is_prime:
+#         print("Prime")
+#     else:
+#         print("Not Prime")
 
 
 
