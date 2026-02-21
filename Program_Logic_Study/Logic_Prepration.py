8e7b1100eaeb9971570e9c8520c7656ea71d7561
Anjana P R <pranjana333@gmail.com>
2026-02-21 22:35:31 +0530

    Add initial implementation of two_sum function

---
+++ Program_Logic_Study/Logic_Prepration.py
@@ -0,0 +1,14 @@
+# 1. Two sum 
+def two_sum(num, target):
+    n = len(num)
+    for i in range(n):
+        for j in range(i+1, n):
+            if num[i]+num[j] == target:
+                return[i,j]
+
+
+    return []
+num = [2,5,6,7,8,10]
+target = 7
+result = two_sum(num, target)
+print(result)
