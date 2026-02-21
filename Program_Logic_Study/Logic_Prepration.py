651f9a9011ceecb43f1ab6650f9bdfbad62fe716
Anjana P R <pranjana333@gmail.com>
2026-02-21 22:38:02 +0530

    Kadane’s Algorithm – Maximum Subarray Sum

--- Program_Logic_Study/Logic_Prepration.py
+++ Program_Logic_Study/Logic_Prepration.py
@@ -1,12 +1,14 @@
 # 1. Two sum 
-def two_sum(num, target):
-    n = len(num)
-    for i in range(n):
-        for j in range(i+1, n):
-            if num[i]+num[j] == target:
-                return[i,j]
-    return []
-num = [2,5,6,7,8,10]
-target = 13
-result = two_sum(num, target)
-print(result)
+# def two_sum(num, target):
+#     n = len(num)
+#     for i in range(n):
+#         for j in range(i+1, n):
+#             if num[i]+num[j] == target:
+#                 return[i,j]
+#     return []
+# num = [2,5,6,7,8,10]
+# target = 13
+# result = two_sum(num, target)
+# print(result)
+
+#  2. Kadane’s Algorithm – Maximum Subarray Sum
