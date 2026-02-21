3f9b966effa70a54f5c7710be6c357fb0652426d
Anjana P R <pranjana333@gmail.com>
2026-02-21 22:37:01 +0530

    python program to find  Two Sum – Arrays

--- Program_Logic_Study/Logic_Prepration.py
+++ Program_Logic_Study/Logic_Prepration.py
@@ -5,10 +5,8 @@ def two_sum(num, target):
         for j in range(i+1, n):
             if num[i]+num[j] == target:
                 return[i,j]
-
-
     return []
 num = [2,5,6,7,8,10]
-target = 7
+target = 13
 result = two_sum(num, target)
 print(result)
