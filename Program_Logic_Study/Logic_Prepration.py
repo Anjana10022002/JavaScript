22097b6ac94f252559fb28196491261fe4866eea
Anjana P R <pranjana333@gmail.com>
2026-02-22 23:40:05 +0530

    python program to find Kadane’s Algorithm – Maximum Subarray Sum

--- Program_Logic_Study/Logic_Prepration.py
+++ Program_Logic_Study/Logic_Prepration.py
@@ -12,20 +12,17 @@
 # print(result)
 
 #  2. Kadane’s Algorithm – Maximum Subarray Sum
-def max_subarray(nums):
-    n = len(nums)
-    max_sum = nums[0]
-    
-    for i in range(n):
-        current_sum = 0
-        for j in range(i, n):
-            current_sum += nums[j]
-            if current_sum > max_sum:
-                max_sum = current_sum
-                
-    return max_sum
+# def max_subarray(nums):
+#     n = len(nums)
+#     max_sum = nums[0]
+#     for i in range(n):
+#         current_sum = 0
+#         for j in range(i, n):
+#             current_sum += nums[j]
+#             if current_sum > max_sum:
+#                 max_sum = current_sum
+#     return max_sum
+# nums = [-2,1,-3,4,-1,2,1,-5,4]
+# print(max_subarray(nums))
 
-
-# Example
-nums = [-2,1,-3,4,-1,2,1,-5,4]
-print(max_subarray(nums))
+# 3. 
