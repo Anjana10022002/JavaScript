bd02b39013490dca377bf6c0a1a5a3543f09ddeb
Anjana P R <pranjana333@gmail.com>
2026-02-23 23:42:41 +0530

    Change Text on Button Click

---
+++ Program_Logic_Study/DOM_manipulation_practice.jsx
@@ -0,0 +1,13 @@
+// 1. Change Text on Button Click
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+</head>
+<body>
+    <p id="test">Hello</p>
+    <button id="btn">CLick me</button>
+</body>
+</html>
